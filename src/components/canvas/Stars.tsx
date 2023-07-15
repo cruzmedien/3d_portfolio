be1ea9ad36374ecea1467ffca69c5dac48b6d09f
William Cruz <cruzmedien@icloud.com>
2023-07-15 21:23:32 +0200

    End with some bugs in the Types

--- src/components/canvas/Stars.tsx
+++ src/components/canvas/Stars.tsx
@@ -4,12 +4,11 @@ import React, { useState, useRef, Suspense } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { Points, PointMaterial, Preload } from '@react-three/drei';
 import * as random from 'maath/random/dist/maath-random.esm';
-import { Group } from 'three';
 
 type StarsProps = JSX.IntrinsicElements['group'];
 
 const Stars: React.FC<StarsProps> = (props) => {
-  const ref = useRef<Group>(null);
+  const ref = useRef<THREE.Points>(null!);
 
   const [sphere] = useState(() =>
     random.inSphere(new Float32Array(5000), { radius: 1.2 }),
@@ -23,7 +22,6 @@ const Stars: React.FC<StarsProps> = (props) => {
     }
   });
 
-  // @ts-ignore
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
       <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
--- types/common.types.d.ts
+++ types/common.types.d.ts
@@ -1,2 +1,3 @@
 declare module 'react-vertical-timeline-component';
 declare module 'maath/random/dist/maath-random.esm';
+declare module '@react-three/drei';
